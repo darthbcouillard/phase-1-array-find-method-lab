94e991c5dcf4277d4a5d15161a9f6dde2944db07
brent couillard <brentcouillard@yahoo.com>
2022-11-01 11:52:08 -0500

    commit tree e1252efd0fa3c8e6f266662dbdb660dd8886696c

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+
+function superbowlWin(array){
+    let obj = array.find(array => array.result === "W")
+    if (obj){
+        return obj.year
+    }
+    return obj
+
+}
